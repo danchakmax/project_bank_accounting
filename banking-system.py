d8a37a12ccd4eea91a426ad1e35353289997fd36
Maksym <danchakmax@gmail.com>
2024-11-05 18:59:01 +0200

    Add function to save user data in a JSON file

--- banking-system.py
+++ banking-system.py
@@ -12,3 +12,11 @@ class BankSystem:
         except (FileNotFoundError, json.JSONDecodeError):
             pass
         return users
+
+
+    def save_data(self):
+
+        with open("bank_data.jsonl", "w") as file:
+            for user_data in self.users.values():
+                json.dump(user_data, file)
+                file.write("\n")
